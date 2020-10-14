e8e6a31d832be31a92b312b4175bd8e6c63386df
Julia Uvaeva <yajuliya@mail.ru>
2020-10-14 23:39:58 +0300

    fixed js objects part 2 task 07

--- src/ex4_js-objects-part2/task-07.js
+++ src/ex4_js-objects-part2/task-07.js
@@ -1,11 +1,9 @@
 const getModificationOfString = function (string, number) {
-    let newStringWithPoints;
-
-    if (string.length > number) {
-        newStringWithPoints = string.substring(0, number - 1) + '…';
+    if (string.length >= number) {
+        return string.substring(0, number - 1) + '…';
     }
 
-    return newStringWithPoints;
+    return string;
 };
 
 module.exports = getModificationOfString;
