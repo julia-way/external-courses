cca3bad292b85ea3d3e90b63e7e776df37aef700
Julia Uvaeva <yajuliya@mail.ru>
2020-11-22 18:29:06 +0300

    fixed kanban-task-blocks

--- src/kanban/mock.js
+++ src/kanban/mock.js
@@ -13,51 +13,51 @@ const initialData = {
   ],
   ready: [
     {
-        id: 2,
-        title: "Shop page - perfomance issues"
-      },
-      {
-        id: 3,
-        title: "Checkout bugfix"
-      },
-      {
-        id: 4,
-        title: "Shop bug1"
-      },
-      {
-        id: 5,
-        title: "Shop bug2"
-      },
-      {
-        id: 6,
-        title: "Shop bug3"
-      },
-      {
-        id: 7,
-        title: "Shop bug4"
-      },
-      {
-        id: 8,
-        title: "Shop bug5"
-      },
-      {
-        id: 9,
-        title: "Shop bug6"
-      },
-      {
-        id: 10,
-        title: "Shop page - perfomance issues"
-      },   
+      id: 2,
+      title: "Shop page - perfomance issues"
+    },
+    {
+      id: 3,
+      title: "Checkout bugfix"
+    },
+    {
+      id: 4,
+      title: "Shop bug1"
+    },
+    {
+      id: 5,
+      title: "Shop bug2"
+    },
+    {
+      id: 6,
+      title: "Shop bug3"
+    },
+    {
+      id: 7,
+      title: "Shop bug4"
+    },
+    {
+      id: 8,
+      title: "Shop bug5"
+    },
+    {
+      id: 9,
+      title: "Shop bug6"
+    },
+    {
+      id: 10,
+      title: "Shop page - perfomance issues"
+    },
   ],
   inProgress: [
     {
-        id: 11,
-        title: "User page - perfomance issues"
-      },
-      {
-        id: 12,
-        title: "Auth bugfix"
-      },
+      id: 11,
+      title: "User page - perfomance issues"
+    },
+    {
+      id: 12,
+      title: "Auth bugfix"
+    },
   ],
   finished: [
     {
